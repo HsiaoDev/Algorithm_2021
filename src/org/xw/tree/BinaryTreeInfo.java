182e7e32b50626af7b225b27d02675955ed39e32
XW <xiaowen@edsz9.com>
2020-11-23 16:13:30 +0800

    1、添加二叉树的info接口

---
+++ src/org/xw/tree/BinaryTreeInfo.java
@@ -0,0 +1,29 @@
+package org.xw.tree;
+
+public interface BinaryTreeInfo {
+	/**
+	 * 获取跟节点
+	 */
+	Object root();
+	
+	/**
+	 * 获取节点的左child
+	 * @param node
+	 * @return
+	 */
+	Object left(Object node);
+	
+	/**
+	 * 获取节点的右child
+	 * @param node
+	 * @return
+	 */
+	Object right(Object node);
+	
+	/**
+	 * 打印节点
+	 * @param node
+	 * @return
+	 */
+	Object string(Object node);
+}
