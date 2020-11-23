7fe016513ecdb1a76de4453ff474b9064906a540
XW <xiaowen@edsz9.com>
2020-11-23 16:02:39 +0800

    1、完善Person类

--- src/org/xw/tree/Person.java
+++ src/org/xw/tree/Person.java
@@ -1,5 +1,36 @@
 package org.xw.tree;
 
-public class Person {
+// 新建Person类实现了Comparable接口
+public class Person implements Comparable<Person>{
+	// 成全变量
+	private int age;
+	private String name;
+	
+	public Person(int age) {
+		this.age = age;
+	}
+	
+	public Person(int age, String name) {
+		this.age = age;
+		this.name = name;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public int compareTo(Person p) {
+		return age - p.age;
+	}
+	
+	@Override
+	public String toString() {
+		return age + "_" + name;
+	}
 
 }
