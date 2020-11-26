2438c63ff2bcf0d9a28dfef762c936d0618dc801
XW <xiaowen@edsz9.com>
2020-11-26 10:17:07 +0800

    1、优化冒泡排序

--- src/org/xw/sort/BubbleSort.java
+++ src/org/xw/sort/BubbleSort.java
@@ -1,44 +1,53 @@
 package org.xw.sort;
 
+import java.util.Arrays;
+
+
 /**
  * 冒泡排序
- * 参考 https://blog.csdn.net/MoreWindows/article/details/17488865
+ * 优课达：https://zhuanlan.zhihu.com/p/128961788
  */
 public class BubbleSort {
 
 	public static void main(String[] args) {
 		
 		// 创建数组
-		int[] list = {95, 45, 15, 78, 84, 51, 24, 12};
+		int[] array = {95, 45, 15, 78, 84, 51, 24, 12};
 		
 		// 打印排序之前的数组
-		System.out.print("排序前：");
-		for (int i = 0; i < list.length; i++) {
-			System.out.print(list[i] + ", ");
-		}
+		System.out.print(Arrays.toString(array));
 		System.out.print("\n");
 		
 		// 调用冒泡排序方法
-		bubbleSort(list);
+		bubbleSort(array);
 		
 		// 打印排序之后的数组
-		System.out.print("排序后：");
-		for (int i = 0; i < list.length; i++) {
-			System.out.print(list[i] + ", ");
-		}
-		System.out.print("\n");
+		System.out.print(Arrays.toString(array));
 	}
 	
-	public static void bubbleSort(int[] list) {
-		int i, j, temp;
-		for (i = 0; i < (list.length - 1); i++) {
-			for (j = 0; j < ((list.length - 1) - i); j++) {
-				// 1、先写内层循环
-				if (list[j] > list[j + 1]) {
-					// 如果相邻两个数前面的数大于后面的数，则交换两个数
-					temp = list[j];
-					list[j] = list[j + 1];
-					list[j + 1] = temp;
+	/**
+	 * Description: 冒泡排序
+	 * 
+	 * @param array 需要排序的数组
+	 */
+	public static void bubbleSort(int[] array) {
+		if (array == null || array.length <= 1) return;
+		
+		int length = array.length;
+		int temp;
+		
+		// 外层循环控制比较轮数
+		for (int i = 0; i < length; i++) {
+			
+			// 内层循环控制比较次数，每一个内循环都会找到一个较大值
+			// (array.length - 1)防止索引越界，(array.length - 1 - i)较小比较次数
+			for (int j = 0; j < length - 1 - i; j++) {
+				
+				// 如果前面的数大于后面的数，就交换两数
+				if (array[j] > array[j + 1]) {
+					temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
 				}
 			}
 		}
